d42b8e2a4115de416c7852c9f0b9fa08d4b6f22b
hendisantika <hendisantika@yahoo.co.id>
2025-10-12 15:05:24 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/capcay_v3demo7/CapcayV3Demo7Application.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.capcay_v3demo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : capcay_v3-demo7
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/20
+ * Time: 08.38
+ */
+
+@SpringBootApplication
+public class CapcayV3Demo7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapcayV3Demo7Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/capcay_v3demo7/CapcayV3Demo7ApplicationTests.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.capcay_v3demo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : capcay_v3-demo7
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/20
+ * Time: 08.38
+ */
+
+@SpringBootTest
+class CapcayV3Demo7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
